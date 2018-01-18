3c69ba7742be3f7b6779f5ecf23550f48976ccf9
snee <alice85319@gmail.com>
2018-01-19 00:17:59 +0800

    first commit

---
+++ final/Money.swift
@@ -0,0 +1,9 @@
+//
+//  Money.swift
+//  final
+//
+//  Created by user_08 on 2018/1/11.
+//  Copyright © 2018年 snee. All rights reserved.
+//
+
+import Foundation
---
+++ final/moneyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoneyTableViewCell.swift
+//  final
+//
+//  Created by user_08 on 2018/1/11.
+//  Copyright © 2018年 snee. All rights reserved.
+//
+
+import UIKit
+
+class MoneyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
